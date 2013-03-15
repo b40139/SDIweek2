5ed8ec534d7a321f3460009000532c2deac09409
Ken Brock <KenBrock@Kens-MacBook-Pro.local>
2013-03-15 00:06:15 -0400

    update.

--- brock_kenneth_1303_project2.js
+++ brock_kenneth_1303_project2.js
@@ -1,2 +1,46 @@
 //alert("JavaScript works!");
 
+/*
+    Kenneth Brock
+    Project 2
+    SDI 1303
+    Loops, Arrays, and Funtions
+*/
+
+//variables
+var myName = "Kevin";
+var numOfGuests = 12;
+var myFriends = ["Fred", "Joe", "Mike", "Simon"];
+var allowedGuests = true;
+
+//procedure
+var addition = function(invites, noninvites){
+    var outcome = invites+noninvites;
+    console.log ("Kevin's total amount of people that showed up is " + outcome)
+};
+
+addition(11,1);
+
+
+//boolean function
+
+
+
+
+
+
+
+
+//number function
+
+
+
+
+
+//string function
+
+
+
+
+
+//array function
